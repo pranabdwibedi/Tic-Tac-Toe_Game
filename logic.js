53401f59069880df823bb3f20e7ad4e9026d3910
Pranab Kumar Dwibedi <pranabkumardwibedi@gmail.com>
2024-07-21 13:50:41 +0530

    Adding Comments for better understanding

--- logic.js
+++ logic.js
@@ -1,10 +1,11 @@
-//all boxes and reset button are get to acces through JS
+//all button fields and button are get to access through JS
 let boxes = document.querySelectorAll(".boxes");
 let rstBtn = document.querySelector("#reset");
 let msg = document.querySelectorAll(".Msg");
 let newGame = document.querySelector("#newGame")
 
-let currentPlayer = "X";//always first turn for player 'X'
+//always first turn for player 'X'
+let currentPlayer = "X";
 
 
 //These are the winning patterns to be a winner
@@ -19,35 +20,49 @@ let winningPattern = [
     [2, 4, 6]
 ];
 
+
+//This function is used when the player try to reset the game or start a new game
 resetingGame = () =>{
     boxes.forEach((box) =>{
+        //This makes all the boxes empty
         box.innerText = "";
+        //this enable the buttons
         box.disabled = false;
+        //the reset button will again visible
         rstBtn.setAttribute("id", "reset");
+        //The winning msg and new Game are hidden now
         msg[0].setAttribute("class", "Msg");
         msg[1].setAttribute("class", "Msg");
     })
 }
 
+//adding event listener for the two button New Game and Reset
 newGame.addEventListener("click",resetingGame);
 rstBtn.addEventListener("click", resetingGame);
 
+
+//It will check the current state with the winning pattern
 checkWinner = () =>{
     for(let pattern of winningPattern){
+        //storing the values of the boxes according to the pattern
         let pos1 = boxes[pattern[0]].innerText;
         let pos2 = boxes[pattern[1]].innerText;
         let pos3 = boxes[pattern[2]].innerText;
+        //Checking wheather any position is empty or not
         if(pos1 != "" && pos2 != "" && pos3 != "")
             if(pos1 === pos2 && pos2 === pos3){
-                console.log(`winner is ${pos1}`);
+                //printing the winner message
                 msg[0].innerText =  `Congtats the Winner is : ${pos1}`;
+                //the winner msg and new game button will visible
                 msg[0].setAttribute("class", "winMsg");
                 msg[1].setAttribute("class", "winMsg");
+                //the reset button will hidden
                 rstBtn.setAttribute("id", "hiddenReset");
             }
         }
 }
 
+//this makes the alternate turn for each player
 boxes.forEach((box) =>{
     box.addEventListener("click", ()=>{
         if(currentPlayer === "X"){
@@ -59,6 +74,7 @@ boxes.forEach((box) =>{
             currentPlayer = "X";
         }
         box.disabled = true;
+        //after every entry it check wheather the winning pattern is matching or not
         checkWinner();
     })
 })
